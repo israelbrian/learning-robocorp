608034cf9a468a8cf6153329e9148e8ce93d6746
Israel <israel_brian@hotmail.com>
2025-09-23 11:30:20 -0300

    Logging into the intranet
    Nova função 'logging-into-thet-intranet' , aprenendo a usar a função page.fill() para encontrar e preencher os campos de entrada de texto, encontrando os elementos a serem preenchidos atraves dos seletores (ids e afins), as credenciais do login foram colocadas no codigo mas isso não é o indicado por se tratar de uma falaha de segurança gravissima, porém para fins didaticos OK.

--- automation-level01/tasks.py
+++ automation-level01/tasks.py
@@ -8,7 +8,16 @@ def robot_spare_bin_python():
         slowmo=600,
     )
     open_the_intranet_website()
+    login()
 
 def open_the_intranet_website():
     """Navigates to the given URL"""
     browser.goto("https://robotsparebinindustries.com/")
+
+def login():
+    """Fills in the login form and clicks the 'Login' button"""
+    page = browser.page()
+    page.fill("#username", "maria")
+    page.fill("#password", "thoushallnotpass")
+    page.click("button:text('Log in')")
+    
